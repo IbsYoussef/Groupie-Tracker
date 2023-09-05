81a05979689772c7c0773defa8485421c802b0a4
iyoussef <ibraheemyoussefiy@gmail.com>
2023-09-05 14:24:31 +0100

    Basic structure

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type JSONReader struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+func main() {
+
+	API_endpoint, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		log.Print(err)
+	}
+
+	Payload := JSONReader{}
+
+}
