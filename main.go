a54b8854980d74a7f919c88693f42ce976c17434
iyoussef <ibraheemyoussefiy@gmail.com>
2023-09-06 16:54:47 +0100

    Clean up

--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
-	"net/http"
 )
 
-type Artists struct {
-	ID           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
+// type Artists struct {
+// 	ID           int      `json:"id"`
+// 	Image        string   `json:"image"`
+// 	Name         string   `json:"name"`
+// 	Members      []string `json:"members"`
+// 	CreationDate int      `json:"creationDate"`
+// 	FirstAlbum   string   `json:"firstAlbum"`
+// 	Locations    string   `json:"locations"`
+// 	ConcertDates string   `json:"concertDates"`
+// 	Relations    string   `json:"relations"`
+// }
+
+type Jsonreader struct {
+	Name     string `json:"name"`
+	Capacity int    `json:"capacity"`
+	Time     int    `json:"time"`
 }
 
 func main() {
 
-	API_endpoint, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	// API_endpoint, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+
+	jsonstring := `{"name": "battery-sensor", "capacity": "50", "time": "22"}`
+
+	var reader Jsonreader
+	err := json.Unmarshal([]byte(jsonstring), &reader)
 	if err != nil {
-		log.Print(err)
+		log.Fatal(err)
 	}
 
 }
