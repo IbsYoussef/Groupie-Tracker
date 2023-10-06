e6ffa235b6f73bacd87a2d4dd8bb64ea0ca37884
Your Name <you@example.com>
2023-10-06 16:00:56 +0100

    unmarshal er str

--- UJSON/unmarshal.go
+++ UJSON/unmarshal.go
@@ -2,26 +2,51 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
+	"io"
+	"net/http"
 )
 
-type Sensor struct {
-	Name     string `json:"name"`
-	Capacity int    `json:"capacity"`
-	Type     string `json:"type"`
+func JsonArtists() ([]Artist, error) {
+	temp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		return nil, err
+	}
+	defer temp.Body.Close()
+	byteResult, _ := io.ReadAll(temp.Body)
+	var res []Artist
+	err2 := json.Unmarshal(byteResult, &res)
+	if err2 != nil {
+		return nil, err2
+	}
+	return res, nil
 }
 
-func main() {
-
-	jsonString := `{"name": "battery-sensor", "capacity": 50, "type": "Lithium"}`
-	var Reader Sensor
-
-	err := json.Unmarshal([]byte(jsonString), &Reader)
+func JsonConcerts(id string) (Concert, error) {
+	temp, err := http.Get("https://groupietrackers.herokuapp.com/api/relation/" + id)
 	if err != nil {
-		log.Fatal(err)
+		return Concert{}, err
 	}
+	defer temp.Body.Close()
+	var tour Concert
+	byteResult, _ := io.ReadAll(temp.Body)
+	err2 := json.Unmarshal(byteResult, &tour)
+	if err2 != nil {
+		return Concert{}, err2
+	}
+	return tour, nil
+}
 
-	fmt.Printf("%+v\n", Reader)
-
+func JsonLocations() (Place, error) {
+	temp, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
+	if err != nil {
+		return Place{}, err
+	}
+	defer temp.Body.Close()
+	var location Place
+	byteResult, _ := io.ReadAll(temp.Body)
+	err2 := json.Unmarshal(byteResult, &location)
+	if err2 != nil {
+		return Place{}, err
+	}
+	return location, nil
 }
