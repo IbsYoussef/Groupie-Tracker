6754303ee0c6c714f69095581c644dc951b92501
iyoussef <ibraheemyoussefiy@gmail.com>
2026-02-04 22:03:00 +0000

    refactor: simplify landing page handler and rename to index.html
    - Remove unnecessary template data passing (static content)
    - Rename landing.html to index.html (standard convention)
    - Simplify LandingHandler to only serve template without data

--- internal/handlers/landing.go
+++ internal/handlers/landing.go
@@ -6,17 +6,5 @@ import (
 
 // LandingHandler serves the landing page
 func LandingHandler(w http.ResponseWriter, r *http.Request) {
-	// Data to pass to template
-	data := struct {
-		Title       string
-		Description string
-		Year        int
-	}{
-		Title:       "Groupie Tracker - Discover Your Music Universe",
-		Description: "Find artists, explore concerts, powered by Spotify",
-		Year:        2025,
-	}
-
-	// Use the centralized renderTemplate helper
-	renderTemplate(w, "landing.html", data)
+	renderTemplate(w, "index.html", nil)
 }
